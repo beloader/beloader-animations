65bd3bca0b9acdd6fb2dbbf28af27ae3abf7c96b
Liqueur de Toile <contact@liqueurdetoile.com>
2018-04-11 10:05:59 +0200

    chore: update publicpath

--- src/core/publicpath.js
+++ src/core/publicpath.js
@@ -1,7 +1,7 @@
 /**
 *  @file publicpath.js
 *  @author Liqueur de Toile <contact@liqueurdetoile.com>
-*  @licence AGPL-3.0 {@link https://github.com/liqueurdetoile/beloader-animations/blob/master/LICENSE}
+*  @licence AGPL-3.0 {@link https://github.com/liqueurdetoile/beloader/blob/master/LICENSE}
 */
 
 /**
@@ -15,11 +15,25 @@
 let scripts = document.getElementsByTagName('script');
 /** @ignore */
 let r = new RegExp('^(?:[a-z]+:)?//', 'i');
+let wpp;
 
-for (let i = 0; i < scripts.length; i++) {
-  let script = scripts[i];
-  let tmp = script.src.match(/(.+)beloader-animations/);
+export function check(uri) {
+  // Dot not forget to change target string beloader
+  let tmp = uri.match(/((http.+)beloader-animations(?:[@\.\w]+|$))/);
 
   // Only set public path if beloader path is absolute
-  if (tmp && tmp[1] && r.test(tmp[1])) __webpack_public_path__ = tmp[1]; // eslint-disable-line
+  if (tmp && tmp[1] && r.test(tmp[1])) {
+    if (tmp[1].indexOf('dist') >= 0) return tmp[2];
+    return tmp[1] + '/dist/';
+  }
+  return false;
+}
+
+for (let i = 0; i < scripts.length; i++) {
+  wpp = check(scripts[i].src);
+
+  if (wpp) {
+    __webpack_public_path__ = wpp; // eslint-disable-line
+    break;
+  }
 }
